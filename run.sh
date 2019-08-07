eb85cf079a0c5966b4a4336a78d6fd2d65679142
Tom Rothamel <pytom@bishoujo.us>
2019-08-07 02:49:27 -0400

    Start cython bindings.

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+export CUBISM=/home/tom/ab/live2d/Cubism3SDKforNative/
+export RENPY=/home/tom/ab/renpy/
+
+export LD_LIBRARY_PATH="$CUBISM/Core/dll/linux/x86_64/"
+
+python setup.py install
+python main.py "$@"
---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import os
+
+RENPY = os.environ["RENPY"]
+CUBISM = os.environ["CUBISM"]
+
+extensions = [
+    Extension(
+        "live2d", ["live2d.pyx"],
+        include_dirs=[ CUBISM + "/Core/include/" ],
+        libraries=[ "Live2DCubismCore" ],
+        library_dirs=[ CUBISM + "/Core/dll/linux/x86_64/" ],
+        ),
+]
+setup(
+    name="live2d",
+    ext_modules=cythonize(extensions, include_path=[ RENPY, RENPY + "/module" ]),
+)
